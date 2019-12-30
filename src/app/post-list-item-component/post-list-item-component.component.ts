537f5856f849f376bad5df7e6fd3cd25f8bb4fc0
CybeR <cybertheo37@gmail.com>
2019-12-30 20:21:08 +0100

    .

--- src/app/post-list-item-component/post-list-item-component.component.ts
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -11,7 +11,6 @@ export class PostListItemComponentComponent implements OnInit {
     @Input() content: string;
     @Input() loveIts: number;
     @Input() created_at: Date;
-    last_state: number = 0;
 
     constructor() { }
 
@@ -20,11 +19,9 @@ export class PostListItemComponentComponent implements OnInit {
 
     onUp(){
         this.loveIts = this.loveIts + 1;
-        this.last_state = 1;
     }
 
     onDown(){
         this.loveIts = this.loveIts - 1;
-        this.last_state = -1;
     }
 }
